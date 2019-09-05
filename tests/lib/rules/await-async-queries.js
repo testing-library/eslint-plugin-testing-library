1c47f6be14fc269ba0bb2dac4da0a016ae3dc300
Mario Beltrán Alarcón <belco90@gmail.com>
2019-09-05 18:07:11 +0200

    First approach for await-async-queries implementation

---
+++ tests/lib/rules/await-async-queries.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/await-async-queries');
+const RuleTester = require('eslint').RuleTester;
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 } });
+ruleTester.run('await-async-queries', rule, {
+  valid: [
+    {
+      code: `async () => {
+        const foo = await findByText('foo')
+      }
+      `,
+    },
+    {
+      code: `async () => {
+        doSomething()
+        const foo = await findByText('foo')
+      }
+      `,
+    },
+    {
+      code: `async () => {
+        doSomething()
+        const foo = await findAllByText('foo')
+      }
+      `,
+    },
+    {
+      code: `anArrowFunction = () => findByText('foo')`,
+    },
+    {
+      code: `function foo() {return findByText('foo')}`,
+    },
+    {
+      code: `function foo() {
+        const promise = findByText('foo')
+        return promise
+      }`,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `async () => {
+        const foo = findByText('foo')
+      }
+      `,
+      errors: [
+        {
+          message: '`findByText` must have `await` operator',
+        },
+      ],
+    },
+    {
+      code: `async () => {
+        doSomething()
+        const foo = findByText('foo')
+      }
+      `,
+      errors: [
+        {
+          message: '`findByText` must have `await` operator',
+        },
+      ],
+    },
+    {
+      code: `async () => {
+        doSomething()
+        const foo = findAllByText('foo')
+      }
+      `,
+      errors: [
+        {
+          message: '`findAllByText` must have `await` operator',
+        },
+      ],
+    },
+  ],
+});
