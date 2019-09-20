962c13a112e6b6df01a32e7ae12ec0aaecfbc72b
Mario Beltrán Alarcón <belco90@gmail.com>
2019-09-20 22:43:27 +0200

    Add configs for angular, react and vue

--- tests/index.js
+++ tests/index.js
@@ -6,4 +6,16 @@ describe('shareable configs', () => {
   it('should export proper "recommended" config', () => {
     expect(configs.recommended).toMatchSnapshot();
   });
+
+  it('should export proper "angular" config', () => {
+    expect(configs.angular).toMatchSnapshot();
+  });
+
+  it('should export proper "react" config', () => {
+    expect(configs.react).toMatchSnapshot();
+  });
+
+  it('should export proper "vue" config', () => {
+    expect(configs.vue).toMatchSnapshot();
+  });
 });
