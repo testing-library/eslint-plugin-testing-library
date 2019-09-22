dba31e963ab2353182d1bead4b97ff75acf2678b
Mario Beltrán Alarcón <belco90@gmail.com>
2019-09-22 17:12:24 +0200

    Simplify configs tests cases with each util

--- tests/index.js
+++ tests/index.js
@@ -3,19 +3,10 @@
 const { configs } = require('../lib');
 
 describe('shareable configs', () => {
-  it('should export proper "recommended" config', () => {
-    expect(configs.recommended).toMatchSnapshot();
-  });
-
-  it('should export proper "angular" config', () => {
-    expect(configs.angular).toMatchSnapshot();
-  });
-
-  it('should export proper "react" config', () => {
-    expect(configs.react).toMatchSnapshot();
-  });
-
-  it('should export proper "vue" config', () => {
-    expect(configs.vue).toMatchSnapshot();
-  });
+  it.each(['recommended', 'angular', 'react', 'vue'])(
+    'should export proper "%s" config',
+    configName => {
+      expect(configs[configName]).toMatchSnapshot();
+    }
+  );
 });
