9329ed6151eabb72678f4dd04cc82ddac855e811
Mario Beltrán Alarcón <belco90@gmail.com>
2019-09-30 15:01:36 +0200

    Feature/rule no dom import (#11)
    * feat: add no-dom-import rule
    
    * test: add no-dom-import tests
    
    * docs: add no-dom-import docs
    
    * docs: update no-dom-import docs

---
+++ tests/lib/rules/no-dom-import.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const rule = require('../../../lib/rules/no-dom-import');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 2015, sourceType: 'module' },
+});
+
+ruleTester.run('no-dom-import', rule, {
+  valid: [
+    { code: 'import { foo } from "foo"' },
+    { code: 'import "foo"' },
+    { code: 'import { fireEvent } from "react-testing-library"' },
+    { code: 'import * as testing from "react-testing-library"' },
+    { code: 'import { fireEvent } from "@testing-library/react"' },
+    { code: 'import * as testing from "@testing-library/react"' },
+    { code: 'import "react-testing-library"' },
+    { code: 'import "@testing-library/react"' },
+    { code: 'const { foo } = require("foo")' },
+    { code: 'require("foo")' },
+    { code: 'require("")' },
+    { code: 'require()' },
+    { code: 'const { fireEvent } = require("react-testing-library")' },
+    { code: 'const { fireEvent } = require("@testing-library/react")' },
+    { code: 'require("react-testing-library")' },
+    { code: 'require("@testing-library/react")' },
+  ],
+  invalid: [
+    {
+      code: 'import { fireEvent } from "dom-testing-library"',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'import * as testing from "dom-testing-library"',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'import { fireEvent } from "@testing-library/dom"',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'import * as testing from "@testing-library/dom"',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'import "dom-testing-library"',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'import "@testing-library/dom"',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'const { fireEvent } = require("dom-testing-library")',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'const { fireEvent } = require("@testing-library/dom")',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'require("dom-testing-library")',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+    {
+      code: 'require("@testing-library/dom")',
+      errors: [
+        {
+          messageId: 'noDomImport',
+        },
+      ],
+    },
+  ],
+});
