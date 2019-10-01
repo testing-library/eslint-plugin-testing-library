0b79ffe835a53596ad70efc77cd05af29a13f8e1
Mario Beltrán Alarcón <belco90@gmail.com>
2019-10-01 15:19:32 +0200

    test: improve exported rules check
    With this improvement the test will check that all the rules available under 'lib/rules' folder are properly exposed in the plugin.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const { configs, rules } = require('../lib');
+const fs = require('fs');
+const path = require('path');
+
+const rulesModules = fs.readdirSync(path.join(__dirname, '/lib/rules'));
 
 it('should export proper rules', () => {
-  expect(Object.keys(rules)).toMatchSnapshot();
+  const availableRules = rulesModules.map(module => module.replace('.js', ''));
+  expect(Object.keys(rules)).toEqual(availableRules);
 });
 
 it.each(['recommended', 'angular', 'react', 'vue'])(
