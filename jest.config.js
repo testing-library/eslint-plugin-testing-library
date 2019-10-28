01bd33b389da087fe49411ce212dfdb411f9dad8
Thomas Lombart <t.lombart97@gmail.com>
2019-10-28 20:47:21 +0100

    test: enforce 100% coverage on CI (#37)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   testMatch: ['**/tests/**/*.js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
