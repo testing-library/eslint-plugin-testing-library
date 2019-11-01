887a35c5b86f4202bcb8cc8568c66dce4c12dd4a
Adrià Fontcuberta <afontcu@gmail.com>
2019-11-01 20:03:45 +0100

    fix(prefer-explicit-assert): avoid raising error on destructuring

--- tests/lib/rules/prefer-explicit-assert.js
+++ tests/lib/rules/prefer-explicit-assert.js
@@ -50,6 +50,15 @@ ruleTester.run('prefer-explicit-assert', rule, {
     {
       code: `getByIcon('foo')`, // custom `getBy` query not extended through options
     },
+    {
+      code: `const { getByText } = render()`,
+    },
+    {
+      code: `it('test', () => { const { getByText } = render() })`,
+    },
+    {
+      code: `it('test', () => { const [ getByText ] = render() })`,
+    },
   ],
 
   invalid: [
