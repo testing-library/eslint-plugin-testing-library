c5b413abd883fd874cda04184423a02ab1ca3cce
Mario Beltrán Alarcón <belco90@gmail.com>
2020-03-14 22:27:51 +0100

    feat(prefer-wait-for): new rule prefer-wait-for

---
+++ tests/lib/rules/prefer-wait-for.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const rule = require('../../../lib/rules/prefer-wait-for');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
+});
+ruleTester.run('prefer-wait-for', rule, {
+  valid: [
+    {
+      code: `async () => {
+        await waitFor(() => {});
+      }`,
+    },
+    {
+      code: `async () => {
+        await waitForElementToBeRemoved(() => {});
+      }`,
+    },
+    {
+      code: `async () => {
+        await waitForSomethingElse(() => {});
+      }`,
+    },
+    {
+      code: `import { wait } from 'some-testing-library'`,
+    },
+    {
+      code: `import { waitForElement } from 'some-testing-library'`,
+    },
+    {
+      code: `import { waitForDomChange } from 'some-testing-library'`,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `async () => {
+        await wait();
+      }`,
+      errors: [
+        {
+          messageId: 'preferWaitFor',
+          line: 2,
+          column: 15,
+        },
+      ],
+      output: `async () => {
+        await waitFor(() => {});
+      }`,
+    },
+    {
+      code: `async () => {
+        await wait(() => {});
+      }`,
+      errors: [
+        {
+          messageId: 'preferWaitFor',
+          line: 2,
+          column: 15,
+        },
+      ],
+      output: `async () => {
+        await waitFor(() => {});
+      }`,
+    },
+    {
+      code: `async () => {
+        await waitForElement(() => {});
+      }`,
+      errors: [
+        {
+          messageId: 'preferWaitFor',
+          line: 2,
+          column: 15,
+        },
+      ],
+      output: `async () => {
+        await waitFor(() => {});
+      }`,
+    },
+    {
+      code: `async () => {
+        await waitForDomChange();
+      }`,
+      errors: [
+        {
+          messageId: 'preferWaitFor',
+          line: 2,
+          column: 15,
+        },
+      ],
+      output: `async () => {
+        await waitFor(() => {});
+      }`,
+    },
+    {
+      code: `async () => {
+        await waitForDomChange(mutationObserverOptions);
+      }`,
+      errors: [
+        {
+          messageId: 'preferWaitFor',
+          line: 2,
+          column: 15,
+        },
+      ],
+      output: `async () => {
+        await waitFor(() => {}, mutationObserverOptions);
+      }`,
+    },
+    {
+      code: `async () => {
+        await waitForDomChange({ options: true });
+      }`,
+      errors: [
+        {
+          messageId: 'preferWaitFor',
+          line: 2,
+          column: 15,
+        },
+      ],
+      output: `async () => {
+        await waitFor(() => {}, { options: true });
+      }`,
+    },
+  ],
+});
