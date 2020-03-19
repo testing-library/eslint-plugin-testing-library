506f95662e317002c564bf0baf86c964d75cac6c
Mario Beltrán Alarcón <belco90@gmail.com>
2020-03-19 21:15:52 +0100

    fix(prefer-wait-for): fix single imports

--- tests/lib/rules/prefer-wait-for.js
+++ tests/lib/rules/prefer-wait-for.js
@@ -23,7 +23,7 @@ ruleTester.run('prefer-wait-for', rule, {
       }`,
     },
     {
-      code: `import { wait, render } from '@testing-library/foo';
+      code: `import { render } from '@testing-library/foo';
       import { waitForSomethingElse } from 'other-module';
       
       async () => {
@@ -35,45 +35,55 @@ ruleTester.run('prefer-wait-for', rule, {
   invalid: [
     {
       code: `import { wait, render } from '@testing-library/foo';
-      
+
       async () => {
         await wait();
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { waitFor, render } from '@testing-library/foo';
-      
+
       async () => {
         await waitFor(() => {});
       }`,
     },
     {
-      code: `import { render, wait } from '@testing-library/foo';
-      
+      code: `import { render, wait } from '@testing-library/foo'
+
       async () => {
         await wait(() => {});
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { render, waitFor } from '@testing-library/foo'
-      
+
       async () => {
         await waitFor(() => {});
       }`,
     },
     {
       code: `import { render, wait, screen } from '@testing-library/foo'
-      
+
       async () => {
         await wait(function cb() {
           doSomething();
@@ -81,13 +91,18 @@ ruleTester.run('prefer-wait-for', rule, {
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { render, waitFor, screen } from '@testing-library/foo'
-      
+
       async () => {
         await waitFor(function cb() {
           doSomething();
@@ -96,26 +111,31 @@ ruleTester.run('prefer-wait-for', rule, {
     },
     {
       code: `import { render, waitForElement, screen } from '@testing-library/foo'
-      
+
       async () => {
         await waitForElement(() => {});
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { render, waitFor, screen } from '@testing-library/foo'
-      
+
       async () => {
         await waitFor(() => {});
       }`,
     },
     {
       code: `import { waitForElement } from '@testing-library/foo';
-      
+
       async () => {
         await waitForElement(function cb() {
           doSomething();
@@ -123,13 +143,18 @@ ruleTester.run('prefer-wait-for', rule, {
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { waitFor } from '@testing-library/foo';
-      
+
       async () => {
         await waitFor(function cb() {
           doSomething();
@@ -138,59 +163,74 @@ ruleTester.run('prefer-wait-for', rule, {
     },
     {
       code: `import { waitForDomChange } from '@testing-library/foo';
-      
+
       async () => {
         await waitForDomChange();
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { waitFor } from '@testing-library/foo';
-      
+
       async () => {
         await waitFor(() => {});
       }`,
     },
     {
       code: `import { waitForDomChange } from '@testing-library/foo';
-      
+
       async () => {
         await waitForDomChange(mutationObserverOptions);
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { waitFor } from '@testing-library/foo';
-      
+
       async () => {
         await waitFor(() => {}, mutationObserverOptions);
       }`,
     },
     {
       code: `import { waitForDomChange } from '@testing-library/foo';
-      
+
       async () => {
-        await waitForDomChange({ options: true });
+        await waitForDomChange({ timeout: 5000 });
       }`,
       errors: [
         {
-          messageId: 'preferWaitFor',
+          messageId: 'preferWaitForImport',
+          line: 1,
+          column: 1,
+        },
+        {
+          messageId: 'preferWaitForMethod',
           line: 4,
           column: 15,
         },
       ],
       output: `import { waitFor } from '@testing-library/foo';
-      
+
       async () => {
-        await waitFor(() => {}, { options: true });
+        await waitFor(() => {}, { timeout: 5000 });
       }`,
     },
   ],
