a958a3a4a8d146f93f010faf7bffedc908de54cb
Mario Beltrán Alarcón <belco90@gmail.com>
2020-03-28 20:45:13 +0100

    feat: new rule prefer-presence-queries (#98)
    * refactor: repurpose obsolete rule
    
    BREAKING CHANGE: rule `no-get-by-for-checking-element-not-present`
    removed in favour of `prefer-presence-queries`
    
    * docs(prefer-presence-queries): update doc accordingly
    
    * feat(prefer-presence-queries): report presence or absence queries
    
    * test(prefer-presence-queries): add more valid cases
    
    * docs(prefer-presence-queries): fix typos
    
    Co-Authored-By: Ben Monro <ben.monro@gmail.com>
    
    * test(prefer-presence-queries): include correct findBy example
    
    * test(prefer-presence-queries): findBy typo
    
    * feat(prefer-presence-queries): include array queries
    
    * fix(prefer-presence-queries): guard against nil matcher
    
    * docs(prefer-presence-queries): update appearance references
    
    Co-authored-by: Ben Monro <ben.monro@gmail.com>

---
+++ docs/rules/prefer-presence-queries.md
@@ -0,0 +1,67 @@
+# Enforce specific queries when checking element is present or not (prefer-presence-queries)
+
+The (DOM) Testing Library allows to query DOM elements using different types of queries such as `get*` and `query*`. Using `get*` throws an error in case the element is not found, while `query*` returns null instead of throwing (or empty array for `queryAllBy*` ones). These differences are useful in some situations:
+
+- using `getBy*` queries when asserting if element is present, so if the element is not found the error thrown will offer better info than asserting with other queries which will not throw an error.
+- using `queryBy*` queries when asserting if element is not present, so the test doesn't fail immediately when the element is not found and the assertion can be executed properly.
+
+## Rule details
+
+This rule fires whenever:
+
+- `queryBy*` or `queryAllBy*` are used to assert element **is** present with `.toBeInTheDocument()`, `toBeTruthy()` or `.toBeDefined()` matchers or negated matchers from case below.
+- `getBy*` or `getAllBy*` are used to assert element **is not** present with `.toBeNull()` or `.toBeFalsy()` matchers or negated matchers from case above.
+
+Examples of **incorrect** code for this rule:
+
+```js
+test('some test', () => {
+  render(<App />);
+
+  // check element is present with `queryBy*`
+  expect(screen.queryByText('button')).toBeInTheDocument();
+  expect(screen.queryAllByText('button')[0]).toBeTruthy();
+  expect(screen.queryByText('button')).toBeNull();
+  expect(screen.queryAllByText('button')[2]).not.toBeNull();
+  expect(screen.queryByText('button')).not.toBeFalsy();
+
+  // check element is NOT present with `getBy*`
+  expect(screen.getByText('loading')).not.toBeInTheDocument();
+  expect(screen.getAllByText('loading')[1]).not.toBeTruthy();
+  expect(screen.getByText('loading')).not.toBeNull();
+  expect(screen.getAllByText('loading')[3]).toBeNull();
+  expect(screen.getByText('loading')).toBeFalsy();
+});
+```
+
+Examples of **correct** code for this rule:
+
+```js
+test('some test', async () => {
+  render(<App />);
+  // check element is present with `getBy*`
+  expect(screen.getByText('button')).toBeInTheDocument();
+  expect(screen.getAllByText('button')[9]).toBeTruthy();
+  expect(screen.getByText('button')).toBeNull();
+  expect(screen.getAllByText('button')[7]).not.toBeNull();
+  expect(screen.getByText('button')).not.toBeFalsy();
+
+  // check element is NOT present with `queryBy*`
+  expect(screen.queryByText('loading')).not.toBeInTheDocument();
+  expect(screen.queryAllByText('loading')[8]).not.toBeTruthy();
+  expect(screen.queryByText('loading')).not.toBeNull();
+  expect(screen.queryAllByText('loading')[6]).toBeNull();
+  expect(screen.queryByText('loading')).toBeFalsy();
+
+  // `findBy*` queries are out of the scope for this rule
+  const button = await screen.findByText('submit');
+  expect(button).toBeInTheDocument();
+});
+```
+
+## Further Reading
+
+- [Testing Library queries cheatsheet](https://testing-library.com/docs/dom-testing-library/cheatsheet#queries)
+- [Asserting elements are not present](https://testing-library.com/docs/guide-disappearance#asserting-elements-are-not-present)
+- [jest-dom note about using `getBy` within assertions](https://testing-library.com/docs/ecosystem-jest-dom)
+- [Waiting for appearance](https://testing-library.com/docs/guide-disappearance#waiting-for-appearance)
---
+++ tests/lib/rules/prefer-presence-queries.js
@@ -0,0 +1,187 @@
+'use strict';
+
+const RuleTester = require('eslint').RuleTester;
+const rule = require('../../../lib/rules/prefer-presence-queries');
+const { ALL_QUERIES_METHODS } = require('../../../lib/utils');
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 2017, sourceType: 'module' },
+});
+
+const getByQueries = ALL_QUERIES_METHODS.map(method => `get${method}`);
+const getAllByQueries = ALL_QUERIES_METHODS.map(method => `getAll${method}`);
+const queryByQueries = ALL_QUERIES_METHODS.map(method => `query${method}`);
+const queryAllByQueries = ALL_QUERIES_METHODS.map(
+  method => `queryAll${method}`
+);
+
+const allQueryUseInAssertion = queryName => [queryName, `screen.${queryName}`];
+
+const getValidAssertion = (query, matcher) =>
+  allQueryUseInAssertion(query).map(query => ({
+    code: `expect(${query}('Hello'))${matcher}`,
+  }));
+
+const getInvalidAssertion = (query, matcher, messageId) =>
+  allQueryUseInAssertion(query).map(query => ({
+    code: `expect(${query}('Hello'))${matcher}`,
+    errors: [{ messageId }],
+  }));
+
+ruleTester.run('prefer-presence-queries', rule, {
+  valid: [
+    ...getByQueries.reduce(
+      (validRules, queryName) => [
+        ...validRules,
+        ...getValidAssertion(queryName, '.toBeInTheDocument()'),
+        ...getValidAssertion(queryName, '.toBeTruthy()'),
+        ...getValidAssertion(queryName, '.toBeDefined()'),
+        ...getValidAssertion(queryName, '.toBe("foo")'),
+        ...getValidAssertion(queryName, '.toEqual("World")'),
+        ...getValidAssertion(queryName, '.not.toBeFalsy()'),
+        ...getValidAssertion(queryName, '.not.toBeNull()'),
+        ...getValidAssertion(queryName, '.not.toBeDisabled()'),
+        ...getValidAssertion(queryName, '.not.toHaveClass("btn")'),
+      ],
+      []
+    ),
+    ...getAllByQueries.reduce(
+      (validRules, queryName) => [
+        ...validRules,
+        ...getValidAssertion(queryName, '.toBeInTheDocument()'),
+        ...getValidAssertion(queryName, '.toBeTruthy()'),
+        ...getValidAssertion(queryName, '.toBeDefined()'),
+        ...getValidAssertion(queryName, '.toBe("foo")'),
+        ...getValidAssertion(queryName, '.toEqual("World")'),
+        ...getValidAssertion(queryName, '.not.toBeFalsy()'),
+        ...getValidAssertion(queryName, '.not.toBeNull()'),
+        ...getValidAssertion(queryName, '.not.toBeDisabled()'),
+        ...getValidAssertion(queryName, '.not.toHaveClass("btn")'),
+      ],
+      []
+    ),
+    ...queryByQueries.reduce(
+      (validRules, queryName) => [
+        ...validRules,
+        ...getValidAssertion(queryName, '.toBeNull()'),
+        ...getValidAssertion(queryName, '.toBeFalsy()'),
+        ...getValidAssertion(queryName, '.not.toBeInTheDocument()'),
+        ...getValidAssertion(queryName, '.not.toBeTruthy()'),
+        ...getValidAssertion(queryName, '.not.toBeDefined()'),
+        ...getValidAssertion(queryName, '.toEqual("World")'),
+        ...getValidAssertion(queryName, '.not.toHaveClass("btn")'),
+      ],
+      []
+    ),
+    ...queryAllByQueries.reduce(
+      (validRules, queryName) => [
+        ...validRules,
+        ...getValidAssertion(queryName, '.toBeNull()'),
+        ...getValidAssertion(queryName, '.toBeFalsy()'),
+        ...getValidAssertion(queryName, '.not.toBeInTheDocument()'),
+        ...getValidAssertion(queryName, '.not.toBeTruthy()'),
+        ...getValidAssertion(queryName, '.not.toBeDefined()'),
+        ...getValidAssertion(queryName, '.toEqual("World")'),
+        ...getValidAssertion(queryName, '.not.toHaveClass("btn")'),
+      ],
+      []
+    ),
+    {
+      code: 'const el = getByText("button")',
+    },
+    {
+      code: 'const el = queryByText("button")',
+    },
+    {
+      code:
+        'expect(getByNonTestingLibraryQuery("button")).not.toBeInTheDocument()',
+    },
+    {
+      code:
+        'expect(queryByNonTestingLibraryQuery("button")).toBeInTheDocument()',
+    },
+    {
+      code: `async () => {
+        const el = await findByText('button')
+        expect(el).toBeInTheDocument()
+      }`,
+    },
+    // some weird examples after here to check guard against parent nodes
+    {
+      code: 'expect(getByText("button")).not()',
+    },
+    {
+      code: 'expect(queryByText("button")).not()',
+    },
+  ],
+  invalid: [
+    ...getByQueries.reduce(
+      (invalidRules, queryName) => [
+        ...invalidRules,
+        ...getInvalidAssertion(queryName, '.toBeNull()', 'absenceQuery'),
+        ...getInvalidAssertion(queryName, '.toBeFalsy()', 'absenceQuery'),
+        ...getInvalidAssertion(
+          queryName,
+          '.not.toBeInTheDocument()',
+          'absenceQuery'
+        ),
+        ...getInvalidAssertion(queryName, '.not.toBeTruthy()', 'absenceQuery'),
+        ...getInvalidAssertion(queryName, '.not.toBeDefined()', 'absenceQuery'),
+      ],
+      []
+    ),
+    ...getAllByQueries.reduce(
+      (invalidRules, queryName) => [
+        ...invalidRules,
+        ...getInvalidAssertion(queryName, '.toBeNull()', 'absenceQuery'),
+        ...getInvalidAssertion(queryName, '.toBeFalsy()', 'absenceQuery'),
+        ...getInvalidAssertion(
+          queryName,
+          '.not.toBeInTheDocument()',
+          'absenceQuery'
+        ),
+        ...getInvalidAssertion(queryName, '.not.toBeTruthy()', 'absenceQuery'),
+        ...getInvalidAssertion(queryName, '.not.toBeDefined()', 'absenceQuery'),
+      ],
+      []
+    ),
+    {
+      code: 'expect(screen.getAllByText("button")[1]).not.toBeInTheDocument()',
+      errors: [{ messageId: 'absenceQuery' }],
+    },
+    ...queryByQueries.reduce(
+      (validRules, queryName) => [
+        ...validRules,
+        ...getInvalidAssertion(queryName, '.toBeTruthy()', 'presenceQuery'),
+        ...getInvalidAssertion(queryName, '.toBeDefined()', 'presenceQuery'),
+        ...getInvalidAssertion(
+          queryName,
+          '.toBeInTheDocument()',
+          'presenceQuery'
+        ),
+        ...getInvalidAssertion(queryName, '.not.toBeFalsy()', 'presenceQuery'),
+        ...getInvalidAssertion(queryName, '.not.toBeNull()', 'presenceQuery'),
+      ],
+      []
+    ),
+    ...queryAllByQueries.reduce(
+      (validRules, queryName) => [
+        ...validRules,
+        ...getInvalidAssertion(queryName, '.toBeTruthy()', 'presenceQuery'),
+        ...getInvalidAssertion(queryName, '.toBeDefined()', 'presenceQuery'),
+        ...getInvalidAssertion(
+          queryName,
+          '.toBeInTheDocument()',
+          'presenceQuery'
+        ),
+        ...getInvalidAssertion(queryName, '.not.toBeFalsy()', 'presenceQuery'),
+        ...getInvalidAssertion(queryName, '.not.toBeNull()', 'presenceQuery'),
+      ],
+      []
+    ),
+    {
+      code: 'expect(screen.queryAllByText("button")[1]).toBeInTheDocument()',
+      errors: [{ messageId: 'presenceQuery' }],
+    },
+  ],
+});
