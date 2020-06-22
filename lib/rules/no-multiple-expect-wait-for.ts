a1ce5296cc8189135fbd9b66a5cc40a34ed595ea
renatoagds <renato_0603@hotmail.com>
2020-06-22 00:27:31 -0300

    feat: add no-multiple-assertion-wait-for logic

--- lib/rules/no-multiple-expect-wait-for.ts
+++ lib/rules/no-multiple-expect-wait-for.ts
@@ -1,5 +1,55 @@
-export { ESLintUtils } from '@typescript-eslint/experimental-utils'
+import { ESLintUtils, TSESTree } from '@typescript-eslint/experimental-utils'
+import { getDocsUrl } from '../utils'
+import { isBlockStatement } from '../node-utils'
 
 export const RULE_NAME = 'no-multiple-expect-wait-for';
 
-export default () => {}
+const WAIT_EXPRESSION_QUERY =
+  'CallExpression[callee.name=/^(waitFor)$/]';
+
+export type MessageIds = 'noMultipleAssertionWaitFor';
+type Options = [];
+
+export default ESLintUtils.RuleCreator(getDocsUrl)<Options, MessageIds>({
+  name: RULE_NAME,
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description:
+        "It's preferred to avoid multiple assertions in `waitFor`",
+      category: 'Best Practices',
+      recommended: false,
+    },
+    messages: {
+      noMultipleAssertionWaitFor: 'Avoid use multiple assertions to `{{ methodName }}`',
+    },
+    fixable: null,
+    schema: [],
+  },
+  defaultOptions: [],
+  create: function(context) {
+    function reporttMultipleAssertion(
+      node: TSESTree.BlockStatement
+    ) {
+      if (isBlockStatement(node) && node.body.length > 1) {
+        const waitForCall: TSESTree.CallExpression = node.parent.parent as TSESTree.CallExpression
+        const waitForIdentifier: TSESTree.Identifier = waitForCall.callee as TSESTree.Identifier
+        const methodName = waitForIdentifier.name
+
+        context.report({
+          node,
+          loc: node.loc.start,
+          messageId: 'noMultipleAssertionWaitFor',
+          data: {
+            methodName,
+          },
+        });
+      }
+    }
+
+    return {
+      [`${WAIT_EXPRESSION_QUERY} > ArrowFunctionExpression > BlockStatement`]: reporttMultipleAssertion,
+      [`${WAIT_EXPRESSION_QUERY} > FunctionExpression > BlockStatement`]: reporttMultipleAssertion,
+    };
+  }
+})
