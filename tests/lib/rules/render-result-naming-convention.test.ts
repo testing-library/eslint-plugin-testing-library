fe4be397386e8127fe0c69865529544f04f72261
Mario Beltrán Alarcón <belco90@gmail.com>
2020-06-30 22:25:56 +0200

    test: first round

---
+++ tests/lib/rules/render-result-naming-convention.test.ts
@@ -0,0 +1,202 @@
+import { createRuleTester } from '../test-utils';
+import rule, {
+  RULE_NAME,
+} from '../../../lib/rules/render-result-naming-convention';
+
+const ruleTester = createRuleTester({
+  ecmaFeatures: {
+    jsx: true,
+  },
+});
+
+ruleTester.run(RULE_NAME, rule, {
+  valid: [
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        test('should not report straight destructured render result', () => {
+          const { rerender, getByText } = render(<SomeComponent />);
+          const button = getByText('some button');
+        });
+      `,
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        test('should not report straight render result called "utils"', async () => {
+          const utils = render(<SomeComponent />);
+          await utils.findByRole('button');
+        });
+      `,
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        test('should not report straight render result called "view"', async () => {
+          const view = render(<SomeComponent />);
+          await view.findByRole('button');
+        });
+      `,
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        const setup = () => render(<SomeComponent />);
+        
+        test('should not report destructured render result from wrapping function', () => {
+          const { rerender, getByText } = setup();
+          const button = getByText('some button');
+        });
+      `,
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        const setup = () => render(<SomeComponent />);
+        
+        test('should not report render result called "utils" from wrapping function', async () => {
+          const utils = setup();
+          await utils.findByRole('button');
+        });
+      `,
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        const setup = () => render(<SomeComponent />);
+        
+        test('should not report render result called "view" from wrapping function', async () => {
+          const view = setup();
+          await view.findByRole('button');
+        });
+      `,
+    },
+    {
+      code: `
+        import { render } from '@foo/bar';
+        
+        test('should not report from render not related to testing library', () => {
+          const wrapper = render(<SomeComponent />);
+          const button = wrapper.getByText('some button');
+        });
+      `,
+    },
+    {
+      code: `
+        import { render } from '@foo/bar';
+        
+        test('should not report from render not imported from testing library', () => {
+          const wrapper = render(<SomeComponent />);
+          const button = wrapper.getByText('some button');
+        });
+      `,
+    },
+    {
+      code: `
+        function render() {
+          return 'whatever';
+        }
+        
+        test('should not report from custom render not related to testing library', () => {
+          const wrapper = render(<SomeComponent />);
+          const button = wrapper.getByText('some button');
+        });
+      `,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        test('should report straight render result called "wrapper"', async () => {
+          const wrapper = render(<SomeComponent />);
+          await wrapper.findByRole('button');
+        });
+      `,
+      errors: [
+        {
+          messageId: 'renderResultNamingConvention',
+          line: 4,
+          column: 17,
+        },
+      ],
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        test('should report straight render result called "component"', async () => {
+          const component = render(<SomeComponent />);
+          await component.findByRole('button');
+        });
+      `,
+      errors: [
+        {
+          messageId: 'renderResultNamingConvention',
+          line: 4,
+          column: 17,
+        },
+      ],
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        test('should report straight render result called "notValidName"', async () => {
+          const notValidName = render(<SomeComponent />);
+          await notValidName.findByRole('button');
+        });
+      `,
+      errors: [
+        {
+          messageId: 'renderResultNamingConvention',
+          line: 4,
+          column: 17,
+        },
+      ],
+    },
+    {
+      code: `
+        import { render as testingLibraryRender } from '@testing-library/react';
+        
+        test('should report renamed render result called "wrapper"', async () => {
+          const wrapper = testingLibraryRender(<SomeComponent />);
+          await wrapper.findByRole('button');
+        });
+      `,
+      errors: [
+        {
+          messageId: 'renderResultNamingConvention',
+          line: 4,
+          column: 17,
+        },
+      ],
+    },
+    {
+      code: `
+        import { render } from '@testing-library/react';
+        
+        const setup = () => render(<SomeComponent />);
+
+        test('should report render result called "wrapper" from wrapping function', async () => {
+          const wrapper = setup();
+          await wrapper.findByRole('button');
+        });
+      `,
+      errors: [
+        {
+          messageId: 'renderResultNamingConvention',
+          line: 4,
+          column: 17,
+        },
+      ],
+    },
+  ],
+});
