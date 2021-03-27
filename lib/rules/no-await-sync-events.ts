21a98dada30549a80366364a24273cbc11db5dba
Mario Beltrán Alarcón <belco90@gmail.com>
2021-03-27 18:08:31 +0100

    refactor: use new rule creator

--- lib/rules/no-await-sync-events.ts
+++ lib/rules/no-await-sync-events.ts
@@ -1,16 +1,15 @@
-import {
-  ASTUtils,
-  ESLintUtils,
-  TSESTree,
-} from '@typescript-eslint/experimental-utils';
-import { getDocsUrl, EVENTS_SIMULATORS } from '../utils';
+import { ASTUtils, TSESTree } from '@typescript-eslint/experimental-utils';
+import { EVENTS_SIMULATORS } from '../utils';
 import { isObjectExpression, isProperty } from '../node-utils';
+import { createTestingLibraryRule } from '../create-testing-library-rule';
+
 export const RULE_NAME = 'no-await-sync-events';
 export type MessageIds = 'noAwaitSyncEvents';
 type Options = [];
 
 const SYNC_EVENTS_REGEXP = new RegExp(`^(${EVENTS_SIMULATORS.join('|')})$`);
-export default ESLintUtils.RuleCreator(getDocsUrl)<Options, MessageIds>({
+
+export default createTestingLibraryRule<Options, MessageIds>({
   name: RULE_NAME,
   meta: {
     type: 'problem',
@@ -20,7 +19,8 @@ export default ESLintUtils.RuleCreator(getDocsUrl)<Options, MessageIds>({
       recommended: 'error',
     },
     messages: {
-      noAwaitSyncEvents: '`{{ name }}` does not need `await` operator',
+      noAwaitSyncEvents:
+        '`{{ name }}` is sync and does not need `await` operator',
     },
     fixable: null,
     schema: [],
@@ -28,10 +28,12 @@ export default ESLintUtils.RuleCreator(getDocsUrl)<Options, MessageIds>({
   defaultOptions: [],
 
   create(context) {
-    // userEvent.type() is an exception, which returns a
-    // Promise. But it is only necessary to wait when delay
-    // option is specified. So this rule has a special exception
-    // for the case await userEvent.type(element, 'abc', {delay: 1234})
+    // userEvent.type() and userEvent.keyboard() are exceptions, which returns a
+    // Promise. But it is only necessary to wait when delay option other than 0
+    // is specified. So this rule has a special exception
+    // for the case await:
+    //  - userEvent.type(element, 'abc', {delay: 1234})
+    //  - userEvent.keyboard('abc', {delay: 1234})
     return {
       [`AwaitExpression > CallExpression > MemberExpression > Identifier[name=${SYNC_EVENTS_REGEXP}]`](
         node: TSESTree.Identifier
