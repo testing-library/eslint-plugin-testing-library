85ad91c3c2f677eca62a80fecb60675c8c2d5cf9
Michaël De Boey <info@michaeldeboey.be>
2021-04-13 00:32:34 +0200

    refactor(node-utils): make isNodeOfType more type-safe

--- lib/node-utils/is-node-of-type.ts
+++ lib/node-utils/is-node-of-type.ts
@@ -3,73 +3,34 @@ import {
   TSESTree,
 } from '@typescript-eslint/experimental-utils';
 
-const isNodeOfType = <NodeOfType extends TSESTree.Node>(
-  nodeType: AST_NODE_TYPES
-) => (node: TSESTree.Node | null | undefined): node is NodeOfType =>
-  node?.type === nodeType;
+const isNodeOfType = <NodeType extends AST_NODE_TYPES>(nodeType: NodeType) => (
+  node: TSESTree.Node | null | undefined
+): node is TSESTree.Node & { type: NodeType } => node?.type === nodeType;
 
-export const isArrayExpression = isNodeOfType<TSESTree.ArrayExpression>(
-  AST_NODE_TYPES.ArrayExpression
-);
-
-export const isArrowFunctionExpression = isNodeOfType<TSESTree.ArrowFunctionExpression>(
+export const isArrayExpression = isNodeOfType(AST_NODE_TYPES.ArrayExpression);
+export const isArrowFunctionExpression = isNodeOfType(
   AST_NODE_TYPES.ArrowFunctionExpression
 );
-
-export const isBlockStatement = isNodeOfType<TSESTree.BlockStatement>(
-  AST_NODE_TYPES.BlockStatement
-);
-
-export const isCallExpression = isNodeOfType<TSESTree.CallExpression>(
-  AST_NODE_TYPES.CallExpression
-);
-
-export const isExpressionStatement = isNodeOfType<TSESTree.ExpressionStatement>(
+export const isBlockStatement = isNodeOfType(AST_NODE_TYPES.BlockStatement);
+export const isCallExpression = isNodeOfType(AST_NODE_TYPES.CallExpression);
+export const isExpressionStatement = isNodeOfType(
   AST_NODE_TYPES.ExpressionStatement
 );
-
-export const isImportDeclaration = isNodeOfType<TSESTree.ImportDeclaration>(
+export const isImportDeclaration = isNodeOfType(
   AST_NODE_TYPES.ImportDeclaration
 );
-
-export const isImportDefaultSpecifier = isNodeOfType<TSESTree.ImportDefaultSpecifier>(
+export const isImportDefaultSpecifier = isNodeOfType(
   AST_NODE_TYPES.ImportDefaultSpecifier
 );
-
-export const isImportNamespaceSpecifier = isNodeOfType<TSESTree.ImportNamespaceSpecifier>(
+export const isImportNamespaceSpecifier = isNodeOfType(
   AST_NODE_TYPES.ImportNamespaceSpecifier
 );
-
-export const isImportSpecifier = isNodeOfType<TSESTree.ImportSpecifier>(
-  AST_NODE_TYPES.ImportSpecifier
-);
-
-export const isJSXAttribute = isNodeOfType<TSESTree.JSXAttribute>(
-  AST_NODE_TYPES.JSXAttribute
-);
-
-export const isLiteral = isNodeOfType<TSESTree.Literal>(AST_NODE_TYPES.Literal);
-
-export const isMemberExpression = isNodeOfType<TSESTree.MemberExpression>(
-  AST_NODE_TYPES.MemberExpression
-);
-
-export const isNewExpression = isNodeOfType<TSESTree.NewExpression>(
-  AST_NODE_TYPES.NewExpression
-);
-
-export const isObjectExpression = isNodeOfType<TSESTree.ObjectExpression>(
-  AST_NODE_TYPES.ObjectExpression
-);
-
-export const isObjectPattern = isNodeOfType<TSESTree.ObjectPattern>(
-  AST_NODE_TYPES.ObjectPattern
-);
-
-export const isProperty = isNodeOfType<TSESTree.Property>(
-  AST_NODE_TYPES.Property
-);
-
-export const isReturnStatement = isNodeOfType<TSESTree.ReturnStatement>(
-  AST_NODE_TYPES.ReturnStatement
-);
+export const isImportSpecifier = isNodeOfType(AST_NODE_TYPES.ImportSpecifier);
+export const isJSXAttribute = isNodeOfType(AST_NODE_TYPES.JSXAttribute);
+export const isLiteral = isNodeOfType(AST_NODE_TYPES.Literal);
+export const isMemberExpression = isNodeOfType(AST_NODE_TYPES.MemberExpression);
+export const isNewExpression = isNodeOfType(AST_NODE_TYPES.NewExpression);
+export const isObjectExpression = isNodeOfType(AST_NODE_TYPES.ObjectExpression);
+export const isObjectPattern = isNodeOfType(AST_NODE_TYPES.ObjectPattern);
+export const isProperty = isNodeOfType(AST_NODE_TYPES.Property);
+export const isReturnStatement = isNodeOfType(AST_NODE_TYPES.ReturnStatement);
