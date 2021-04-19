b14a445cf35184a00c1f15e952fc624bad9f588b
Michaël De Boey <info@michaeldeboey.be>
2021-04-19 12:37:19 +0200

    test: add extra tests (#332)

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,25 +1,67 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { configs, rules } = require('../lib');
-import * as fs from 'fs';
-import * as path from 'path';
+import { existsSync } from 'fs';
+import { resolve } from 'path';
 
-const rulesModules = fs.readdirSync(path.join(__dirname, '../lib/rules'));
+import plugin from '../lib';
 
-it('should export all available rules', () => {
-  const availableRules = rulesModules.map((module) =>
-    module.replace('.ts', '')
-  );
-  expect(Object.keys(rules)).toEqual(availableRules);
+const numberOfRules = 24;
+const ruleNames = Object.keys(plugin.rules);
+
+// eslint-disable-next-line jest/expect-expect
+it('should have a corresponding doc for each rule', () => {
+  ruleNames.forEach((rule) => {
+    const docPath = resolve(__dirname, '../docs/rules', `${rule}.md`);
+
+    if (!existsSync(docPath)) {
+      throw new Error(
+        `Could not find documentation file for rule "${rule}" in path "${docPath}"`
+      );
+    }
+  });
 });
 
-it.each(['dom', 'angular', 'react', 'vue'])(
-  'should export proper "%s" config',
-  (configName) => {
-    expect(configs[configName]).toMatchSnapshot();
+// eslint-disable-next-line jest/expect-expect
+it('should have a corresponding test for each rule', () => {
+  ruleNames.forEach((rule) => {
+    const testPath = resolve(__dirname, './lib/rules/', `${rule}.test.ts`);
 
-    // make sure all enabled rules start by "testing-library/" prefix
-    Object.keys(configs[configName].rules).forEach((ruleEnabled) => {
-      expect(ruleEnabled).toMatch(/^testing-library\/.+$/);
-    });
+    if (!existsSync(testPath)) {
+      throw new Error(
+        `Could not find test file for rule "${rule}" in path "${testPath}"`
+      );
+    }
+  });
+});
+
+// eslint-disable-next-line jest/expect-expect
+it('should have the correct amount of rules', () => {
+  const { length } = ruleNames;
+
+  if (length !== numberOfRules) {
+    throw new Error(
+      `There should be exactly ${numberOfRules} rules, but there are ${length}. If you've added a new rule, please update this number.`
+    );
   }
-);
+});
+
+it('should export configs that refer to actual rules', () => {
+  const allConfigs = plugin.configs;
+
+  expect(allConfigs).toMatchSnapshot();
+  expect(Object.keys(allConfigs)).toEqual(['dom', 'angular', 'react', 'vue']);
+  const allConfigRules = Object.values(allConfigs)
+    .map((config) => Object.keys(config.rules))
+    .reduce((previousValue, currentValue) => [
+      ...previousValue,
+      ...currentValue,
+    ]);
+
+  allConfigRules.forEach((rule) => {
+    const ruleNamePrefix = 'testing-library/';
+    const ruleName = rule.slice(ruleNamePrefix.length);
+
+    expect(rule.startsWith(ruleNamePrefix)).toBe(true);
+    expect(ruleNames).toContain(ruleName);
+
+    expect(() => require(`../lib/rules/${ruleName}`)).not.toThrow();
+  });
+});
