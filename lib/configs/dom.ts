682fd59e0518e67907f392b6d49763da9fd59d64
Michaël De Boey <info@michaeldeboey.be>
2021-04-26 03:22:09 +0200

    chore: automatically generate all configs

--- lib/configs/dom.ts
+++ lib/configs/dom.ts
@@ -1,12 +1,5 @@
-export = {
-  plugins: ['testing-library'],
-  rules: {
-    'testing-library/await-async-query': 'error',
-    'testing-library/await-async-utils': 'error',
-    'testing-library/no-await-sync-query': 'error',
-    'testing-library/no-promise-in-fire-event': 'error',
-    'testing-library/no-wait-for-empty-callback': 'error',
-    'testing-library/prefer-find-by': 'error',
-    'testing-library/prefer-screen-queries': 'error',
-  },
-};
+// THIS CODE WAS AUTOMATICALLY GENERATED
+// DO NOT EDIT THIS CODE BY HAND
+// YOU CAN REGENERATE IT USING yarn generate:configs
+
+export = { plugins: ['testing-library'], rules: {} };
--- lib/configs/vue.ts
+++ lib/configs/vue.ts
@@ -1,18 +1,5 @@
-export = {
-  plugins: ['testing-library'],
-  rules: {
-    'testing-library/await-async-query': 'error',
-    'testing-library/await-async-utils': 'error',
-    'testing-library/no-await-sync-query': 'error',
-    'testing-library/no-promise-in-fire-event': 'error',
-    'testing-library/no-wait-for-empty-callback': 'error',
-    'testing-library/prefer-find-by': 'error',
-    'testing-library/prefer-screen-queries': 'error',
-    'testing-library/await-fire-event': 'error',
-    'testing-library/no-container': 'error',
-    'testing-library/no-debug': 'error',
-    'testing-library/no-dom-import': ['error', 'vue'],
-    'testing-library/no-node-access': 'error',
-    'testing-library/render-result-naming-convention': 'error',
-  },
-};
+// THIS CODE WAS AUTOMATICALLY GENERATED
+// DO NOT EDIT THIS CODE BY HAND
+// YOU CAN REGENERATE IT USING yarn generate:configs
+
+export = { plugins: ['testing-library'], rules: {} };
