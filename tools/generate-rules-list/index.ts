84fe65f98ba075ad03161c82504e8da4b9dd8fef
Michaël De Boey <info@michaeldeboey.be>
2021-06-21 12:14:41 +0200

    refactor: automatically generate the rules-list in README

---
+++ tools/generate-rules-list/index.ts
@@ -0,0 +1,26 @@
+import rules from '../../lib/rules';
+import type { TestingLibraryRuleMetaDocs } from '../../lib/utils';
+
+import { configBadges, emojiKey, RulesList, writeRulesList } from './utils';
+
+export const createRuleLink = (ruleName: string): string =>
+  `[\`testing-library/${ruleName}\`](./docs/rules/${ruleName}.md)`;
+
+export const generateConfigBadges = (
+  recommendedConfig: TestingLibraryRuleMetaDocs<unknown[]>['recommendedConfig']
+): string =>
+  Object.entries(recommendedConfig)
+    .filter(([_, config]) => Boolean(config))
+    .map(([framework]) => configBadges[framework])
+    .join(' ');
+
+const rulesList: RulesList = Object.entries(rules)
+  .sort(([ruleNameA], [ruleNameB]) => ruleNameA.localeCompare(ruleNameB))
+  .map(([name, rule]) => [
+    createRuleLink(name),
+    rule.meta.docs.description,
+    Boolean(rule.meta.fixable) ? emojiKey.fixable : '',
+    generateConfigBadges(rule.meta.docs.recommendedConfig),
+  ]);
+
+writeRulesList(rulesList);
