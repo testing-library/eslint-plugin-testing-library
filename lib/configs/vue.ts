e71fb14f39028a206e9ec8a61c21fbe44a845514
Michaël De Boey <info@michaeldeboey.be>
2021-09-23 17:02:32 +0200

    feat(no-wait-for-multiple-assertions): add to all configs by default
    BREAKING CHANGE: `no-wait-for-multiple-assertions` is now enabled by default in all configs

--- lib/configs/vue.ts
+++ lib/configs/vue.ts
@@ -15,6 +15,7 @@ export = {
     'testing-library/no-node-access': 'error',
     'testing-library/no-promise-in-fire-event': 'error',
     'testing-library/no-wait-for-empty-callback': 'error',
+    'testing-library/no-wait-for-multiple-assertions': 'error',
     'testing-library/prefer-find-by': 'error',
     'testing-library/prefer-screen-queries': 'error',
     'testing-library/render-result-naming-convention': 'error',
