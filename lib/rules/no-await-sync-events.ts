2d8fe9552b8fd8cfa529d955729f85d50a1f61e2
Mario Beltrán Alarcón <belco90@gmail.com>
2022-04-10 23:12:48 +0200

    feat(no-await-sync-events): report only when event module enabled

--- lib/rules/no-await-sync-events.ts
+++ lib/rules/no-await-sync-events.ts
@@ -49,7 +49,9 @@ export default createTestingLibraryRule<Options, MessageIds>({
   },
   defaultOptions: [{ eventModules: VALID_EVENT_MODULES }],
 
-  create(context, _, helpers) {
+  create(context, [options], helpers) {
+    const { eventModules = VALID_EVENT_MODULES } = options;
+
     // userEvent.type() and userEvent.keyboard() are exceptions, which returns a
     // Promise. But it is only necessary to wait when delay option other than 0
     // is specified. So this rule has a special exception for the case await:
@@ -63,11 +65,22 @@ export default createTestingLibraryRule<Options, MessageIds>({
           return;
         }
 
-        const isSimulateEventMethod =
-          helpers.isUserEventMethod(simulateEventFunctionIdentifier) ||
-          helpers.isFireEventMethod(simulateEventFunctionIdentifier);
+        const isUserEventMethod = helpers.isUserEventMethod(
+          simulateEventFunctionIdentifier
+        );
+        const isFireEventMethod = helpers.isFireEventMethod(
+          simulateEventFunctionIdentifier
+        );
+        const isSimulateEventMethod = isUserEventMethod || isFireEventMethod;
 
         if (!isSimulateEventMethod) {
+          return;
+        }
+
+        if (isFireEventMethod && !eventModules.includes('fire-event')) {
+          return;
+        }
+        if (isUserEventMethod && !eventModules.includes('user-event')) {
           return;
         }
 
