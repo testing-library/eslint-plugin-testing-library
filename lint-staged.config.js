7644e3b99fd4890ea7d90d055dbcc8987e9c3b97
Mario Beltrán Alarcón <belco90@gmail.com>
2022-08-18 17:12:12 +0200

    refactor: move lint-staged config to a js file

---
+++ lint-staged.config.js
@@ -0,0 +1,19 @@
+//eslint-disable-next-line @typescript-eslint/no-var-requires
+const { ESLint } = require('eslint');
+
+const removeIgnoredFiles = async (files) => {
+	const eslint = new ESLint();
+	const ignoredFiles = await Promise.all(
+		files.map((file) => eslint.isPathIgnored(file))
+	);
+	const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
+	return filteredFiles.join(' ');
+};
+
+module.exports = {
+	'*.{js,ts}': async (files) => {
+		const filesToLint = await removeIgnoredFiles(files);
+		return [`eslint --max-warnings=0 ${filesToLint}`];
+	},
+	'*': 'prettier --write --ignore-unknown',
+};
