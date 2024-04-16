be6282434b05b12f338d31558d1793b07e3d765d
Mario Beltrán <belco90@gmail.com>
2024-04-16 12:25:17 +0200

    feat: export meta info

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,15 @@
+import packageMetadata from '../package.json';
+
 import configs from './configs';
 import rules from './rules';
 
-export = {
+const plugin = {
+	meta: {
+		name: packageMetadata.name,
+		version: packageMetadata.version,
+	},
 	configs,
 	rules,
 };
+
+export default plugin;
