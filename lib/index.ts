4e1b524793eb71ca332d8e21d4a5bfdcccd1f26a
Mario Beltrán <belco90@gmail.com>
2024-04-21 16:52:41 +0200

    chore: assign plugins to configs properly

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,9 @@ import packageMetadata from '../package.json';
 
 import configs from './configs';
 import rules from './rules';
+import { type SupportedTestingFramework } from './utils';
 
+// TODO: type properly when upgraded to ESLint v9
 const plugin = {
 	meta: {
 		name: packageMetadata.name,
@@ -13,12 +15,13 @@ const plugin = {
 };
 
 // TODO: type this with TSESLint.Linter.RuleEntry when upgraded to ESLint v9
-const pluginConfigs: Record<string, unknown> = {};
-for (const [key, config] of Object.entries(configs)) {
-	pluginConfigs[key] = {
-		...config,
-		plugins: { 'testing-library': plugin },
+Object.keys(plugin.configs).forEach((configKey) => {
+	plugin.configs[configKey as SupportedTestingFramework].plugins = {
+		// TODO: remove ignored error when properly typed with ESLint v9
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		'testing-library': plugin,
 	};
-}
+});
 
 export default plugin;
