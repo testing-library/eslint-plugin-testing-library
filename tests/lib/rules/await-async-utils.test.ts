fd46b968d5955b107a5c084a79a0722353c61536
neriyarden <neriyarden@gmail.com>
2024-07-27 16:46:20 +0300

    feat(await-async-utils): add autofix for references

--- tests/lib/rules/await-async-utils.test.ts
+++ tests/lib/rules/await-async-utils.test.ts
@@ -465,7 +465,7 @@ ruleTester.run(RULE_NAME, rule, {
         import { ${asyncUtil} } from '${testingFramework}';
         test('several ${asyncUtil} utils not handled are invalid', () => {
           const aPromise = ${asyncUtil}(() => getByLabelText('username'));
-          doSomethingElse(aPromise);
+          doSomethingElse(await aPromise);
           await ${asyncUtil}(() => getByLabelText('email'));
         });
       `,
@@ -500,7 +500,7 @@ ruleTester.run(RULE_NAME, rule, {
         import { ${asyncUtil} } from '${testingFramework}';
         test('unhandled expression that evaluates to promise is invalid', () => {
           const aPromise = ${asyncUtil}(() => getByLabelText('username'));
-          doSomethingElse(aPromise);
+          doSomethingElse(await aPromise);
           await ${asyncUtil}(() => getByLabelText('email'));
         });
       `,
