4c6f7c9262faa28ce631fbf379e90d15f117bd08
Gareth Jones <jones258@gmail.com>
2024-08-04 17:58:34 +1200

    chore: enable `eslint-plugin-prettier`

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
 		'plugin:import/recommended',
 		'plugin:jest/recommended',
 		'plugin:jest-formatting/recommended',
-		'prettier',
+		'plugin:prettier/recommended',
 	],
 	rules: {
 		// Base
