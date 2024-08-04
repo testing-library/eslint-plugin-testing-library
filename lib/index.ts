fe554b917ec857f821a1214d9c82629916be2341
Gareth Jones <jones258@gmail.com>
2024-08-04 17:37:26 +1200

    fix: add plugin name and version metadata for ESLint v9

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,16 @@
+import {
+	name as packageName,
+	version as packageVersion,
+} from '../package.json';
+
 import configs from './configs';
 import rules from './rules';
 
 export = {
+	meta: {
+		name: packageName,
+		version: packageVersion,
+	},
 	configs,
 	rules,
 };
