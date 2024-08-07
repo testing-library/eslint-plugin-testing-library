3b766d16c3266e750a3be4f1812c85f711f0b34b
Gareth Jones <jones258@gmail.com>
2024-08-08 07:17:50 +1200

    fix: don't "import" `package.json`

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,12 @@
-import {
-	name as packageName,
-	version as packageVersion,
-} from '../package.json';
-
 import configs from './configs';
 import rules from './rules';
+
+// we can't natively import package.json as tsc will copy it into dist/
+const {
+	name: packageName,
+	version: packageVersion,
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+} = require('../package.json') as { name: string; version: string };
 
 export = {
 	meta: {
