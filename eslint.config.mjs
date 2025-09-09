5d5b4849791ab94dfa02ae545ca7561e94981de2
Mario Beltrán <me@mario.dev>
2025-09-09 23:36:16 +0200

    refactor: replace import with import-x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,33 @@
 // @ts-check
 
-import { fixupConfigRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import prettierConfig from 'eslint-config-prettier/flat';
+import { importX } from 'eslint-plugin-import-x';
 import jest from 'eslint-plugin-jest';
 import * as jestFormatting from 'eslint-plugin-jest-formatting';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-const compat = new FlatCompat({
-	baseDirectory: import.meta.dirname,
-});
-
 const config = defineConfig(
 	js.configs.recommended,
 	tseslint.configs.recommendedTypeChecked,
+	importX.flatConfigs.recommended,
+	importX.flatConfigs.typescript,
 	{
 		name: 'Language options',
+		files: ['**/*.{js,mjs,cjs,ts,mts}'],
 		languageOptions: {
 			globals: {
 				...globals.node,
 			},
+			ecmaVersion: 'latest',
+			sourceType: 'module',
 			parser: tseslint.parser,
 			parserOptions: {
 				projectService: true,
 				tsconfigRootDir: import.meta.dirname,
 			},
-		},
-	},
-	{
-		// TODO: replace import with import-x
-		name: 'Import plugin config',
-		extends: fixupConfigRules(compat.extends('plugin:import/recommended')),
-
-		rules: {
-			'import/order': [
-				'warn',
-				{
-					groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
-					'newlines-between': 'always',
-
-					alphabetize: {
-						order: 'asc',
-						caseInsensitive: false,
-					},
-				},
-			],
-
-			'import/first': 'error',
-			'import/no-empty-named-blocks': 'error',
-			'import/no-extraneous-dependencies': 'error',
-			'import/no-mutable-exports': 'error',
-			'import/no-named-default': 'error',
-			'import/no-relative-packages': 'warn',
 		},
 	},
 	{
@@ -72,49 +45,40 @@ const config = defineConfig(
 				},
 			],
 			'@typescript-eslint/no-use-before-define': 'off',
+
+			// Import
+			'import-x/order': [
+				'warn',
+				{
+					groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+					'newlines-between': 'always',
+
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: false,
+					},
+				},
+			],
+			'import-x/first': 'error',
+			'import-x/no-empty-named-blocks': 'error',
+			'import-x/no-extraneous-dependencies': 'error',
+			'import-x/no-mutable-exports': 'error',
+			'import-x/no-named-default': 'error',
+			'import-x/no-relative-packages': 'warn',
 		},
 	},
 	{
-		name: 'Rule overrides for TypeScript files',
-		files: ['**/*.ts', '**/*.mts'],
+		name: 'Rule overrides only for TypeScript files',
+		files: ['**/*.{ts,mts}'],
 		rules: {
-			// Import
-			// Rules enabled by `import/recommended` but are better handled by
-			// TypeScript and @typescript-eslint.
-			'import/default': 'off',
-			'import/export': 'off',
-			'import/namespace': 'off',
-			'import/no-unresolved': 'off',
+			// Rules enabled by `import-x/recommended` but are better handled by
+			// TypeScript and typescript-eslint.
+			'import-x/default': 'off',
+			'import-x/export': 'off',
+			'import-x/namespace': 'off',
+			'import-x/no-unresolved': 'off',
 		},
 	},
-	// {
-	// 	files: ['**/*.ts'],
-	//
-	// 	extends: fixupConfigRules(
-	// 		compat.extends(
-	// 			'plugin:import/typescript'
-	// 		)
-	// 	),
-	//
-	// 	rules: {
-	// 		'import/default': 'off',
-	// 		'import/export': 'off',
-	// 		'import/namespace': 'off',
-	// 		'import/no-unresolved': 'off',
-	// 	},
-	//
-	// 	settings: {
-	// 		'import/resolver': {
-	// 			node: {
-	// 				extensions: ['.js', '.ts'],
-	// 			},
-	//
-	// 			typescript: {
-	// 				alwaysTryTypes: true,
-	// 			},
-	// 		},
-	// 	},
-	// },
 	{
 		name: 'Jest config',
 		files: ['**/*.test.ts', '**/*.test.js'],
@@ -123,15 +87,15 @@ const config = defineConfig(
 	},
 	{
 		name: 'Plain JS',
-		files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
+		files: ['**/*.{js,mjs,cjs}'],
 		extends: [tseslint.configs.disableTypeChecked],
 	},
 	{
 		name: 'Config files',
-		files: ['./*.js', './*.cjs', './*.mjs', '**/*.config.*js'],
+		files: ['**/*rc*.{js,mjs,cjs}', '**/*.config.{js,cjs,mjs,ts}'],
 		rules: {
 			'@typescript-eslint/no-require-imports': 'off',
-			'import/no-unresolved': 'off',
+			'import-x/no-named-as-default-member': 'off',
 		},
 	},
 	globalIgnores(['**/coverage/', '**/dist/']),
